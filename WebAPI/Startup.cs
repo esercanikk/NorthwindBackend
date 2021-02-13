51db2ca137a92b7ef1c5ee6662b7d0d2333f9def
esercanikk <canikeser@gmail.com>
2021-02-13 15:22:51 +0300

    #4 ServiceTool ,Framework Seviyesinde Core Module,Core Module İmplementasyonu,Service Collection Extension  ve Extension İmplementasyonu

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,3 +1,5 @@
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.Security.Encyption;
 using Core.Utilities.Security.Jwt;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -52,6 +54,11 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
+
+            services.AddDependencyResolvers(new Core.Utilities.IoC.ICoreModule[]
+            {
+                new CoreModule(),
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
