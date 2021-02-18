6468f4ce33defc056a13e1743e48d435b7c0cbe2
esercanikk <canikeser@gmail.com>
2021-02-18 13:50:44 +0300

    #7 PerformanceInterceptionAspect Yazmak

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -3,6 +3,7 @@ using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Performance;
 using Core.Aspects.Autofac.Validation;
 using Core.Aspects.Caching;
 using Core.Aspects.Transaction;
@@ -12,6 +13,7 @@ using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace Business.Concrete
 {
@@ -29,8 +31,10 @@ namespace Business.Concrete
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
+        [PerformanceAspect(5)]
         public IDataResult<List<Product>> GetList()
         {
+            Thread.Sleep(5000);
             return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList());
         }
 
